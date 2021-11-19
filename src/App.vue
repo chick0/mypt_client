c12d1198ca2c41ed6b712e44de2860e58efb3198
chick_0 <chick_0@ch1ck.xyz>
2021-11-19 23:54:19 +0900

    푸터 수정
    간단하게 수정

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,8 @@
 
     <router-view />
 
-    <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item">
-                <router-link to="/" class="nav-link px-2 text-muted">Home</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link to="/contact" class="nav-link px-2 text-muted">Contact</router-link>
-            </li>
-        </ul>
-        <p class="text-center text-muted">© {{ year }} chick_0</p>
+    <footer class="py-3 my-4 text-center">
+        <router-link to="/" class="text-muted">&copy; {{ year }} chick_0</router-link>
     </footer>
 </template>
 
