c7299b62d26ae672828ee2521a95101e64987787
chick_0 <chick_0@ch1ck.xyz>
2022-01-16 02:05:58 +0900

    spinner added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
-
-    <div class="spinner" tabindex="-1" v-if="spinner == true">
-    </div>
-
     <router-view />
-
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { useLoading } from 'vue-loading-overlay';
 import axios from 'axios';
 
 export default {
     setup(){
-        const spinner = ref(false);
+        // 스피너 객체를 보관하는 변수
+        const loading = useLoading();
+        let loadingObj = undefined;
+
+        // 스피너가 돌아야 하는지 여부를 저장하는 변수
+        const spinner = ref();
+
+        // 스피너 핸들러
+        watch(spinner, (need_spin) => {
+            if(need_spin){
+                loadingObj = loading.show({
+                    color: "#FFCC4D",
+                    canCancel: false,
+                });
+            } else {
+                if(loadingObj != undefined){
+                    loadingObj.hide();
+                }
+            }
+        });
 
         // axios 요청 시작시 스피너 보이기
         // 오류나거나 요청이 끝나면 스피터 숨기기
@@ -28,10 +43,6 @@ export default {
         }, error => {
             spinner.value = false;  return Promise.reject(error);
         });
-
-        return {
-            spinner: spinner
-        }
     }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import router from "./router";
 
+import VueLoading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
 import App from './App.vue';
 
 
 const app = createApp(App);
 app.use(router);
+app.use(VueLoading);
 app.mount('#app');
